0d3d8b937fd8d025ad9f5812d651a2d5be6b101f
Clemens Giuliani <clemens@inailuig.it>
2021-02-07 16:05:38 +0100

    inprove bench.py

--- bench.py
+++ bench.py
@@ -5,7 +5,7 @@ import jax.numpy as jnp
 import numpy as np
 from jax.random import threefry2x32_p
 
-num = 2**26
+num = (2**26)*3
 k = jax.random.PRNGKey(123)
 
 key1, key2 = k
@@ -13,7 +13,7 @@ x0 = jax.lax.iota(np.uint32, num).block_until_ready()
 x1 = (x0+num).block_until_ready()
 
 def bench():
-    f = jax.jit(lambda k: jax.random.split(k, 2**26)) # force re-jit; needed cause we change the impl
+    f = jax.jit(lambda k: jax.random.split(k, num)) # force re-jit; needed cause we change the impl
     _ = f(k) # compile
     get_ipython().magic("timeit f(k).block_until_ready()")
 
@@ -22,13 +22,27 @@ def bench2():
     _ = f(key1, key2, x0, x1) # compile
     get_ipython().magic("timeit f(key1, key2, x0, x1)[0].block_until_ready()")
 
+def bench3():
+    # shape 2*num since we want to generate the same number of bits as the other benchs
+    f = jax.jit(lambda k: jax.random.uniform(k, shape=(2*num,))) # force re-jit; needed cause we change the impl
+    _ = f(k) # compile
+    get_ipython().magic("timeit f(k).block_until_ready()")
+
+def bench4():
+    f = jax.jit(lambda k: jax.random.randint(k, shape=(2*num,), minval=0, maxval=256)) # force re-jit; needed cause we change the impl
+    _ = f(k) # compile
+    get_ipython().magic("timeit f(k).block_until_ready()")
+
 
 print("jax:")
 bench()
 bench2()
-
+bench3()
+bench4()
 
 import threefry
 print("\nthreefry:")
 bench()
 bench2()
+bench3()
+bench4()
